3d8af33f456e8f938bceda8024b87e67a45192c8
pabis-code-crafts <pavithrangurusamy21@gmail.com>
2025-03-02 00:16:23 +0000

    feature1 : added patient model

--- bpc/src/main/java/com/example/Main.java
+++ bpc/src/main/java/com/example/Main.java
@@ -1,9 +1,20 @@
 package com.example;
 
+import com.example.data.PatientData;
+
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
         System.err.println("checking if it works");
+        PatientData patient = new PatientData();
+        patient.setName("John Doe");
+        patient.setAddress("123 Main St");
+        patient.setEmail("  [email protected]");
+        patient.setPhone("123-456-7890");   
+        
+        System.out.println("Name: " + patient.getName());
+        System.out.println("Address: " + patient.getAddress());
     }
 }
---
+++ bpc/src/main/java/com/example/data/PatientData.java
@@ -0,0 +1,50 @@
+package com.example.data;
+
+public class PatientData {
+    private String name;
+    private String address;
+    private String email;
+    private String phone;
+    private String dob;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+}
